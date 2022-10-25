345988ec68f298632d0c7c2e94816dde4b171c83
ivanchikap <anton.ivanchik@gmail.com>
2022-10-25 10:36:31 +0300

    add nav script

---
+++ src/assets/js/app.js
@@ -0,0 +1,57 @@
+document.addEventListener('DOMContentLoaded', (e) => {
+
+    //nav
+
+    const nav = document.querySelector('.nav');
+    const navFooter = document.querySelector('.nav--footer');
+
+    nav.addEventListener('click', (e) => {
+        e.preventDefault();
+        if (!e.target.classList.contains('nav__item')) return;
+        addActive(e.target, e.currentTarget);
+
+        const footerNav = document.querySelector('.nav--footer');
+        const [...footerLinks] = footerNav.querySelectorAll('.nav__item');
+
+        const neededEl = footerLinks.find((link) => link.href === e.target.href);
+
+        addActive(neededEl, footerNav);
+
+
+        const blockId = e.target.getAttribute('href').substring(1);
+
+        document.getElementById(blockId).scrollIntoView({
+            behavior: "smooth",
+            block: "start"
+        });
+    });
+
+    navFooter.addEventListener('click', (e) => {
+        e.preventDefault();
+        if (!e.target.classList.contains('nav__item')) return;
+        addActive(e.target, e.currentTarget);
+
+        const headerNav = document.querySelector('.nav');
+        const [...headerLinks] = headerNav.querySelectorAll('.nav__item');
+
+        const neededEl = headerLinks.find((link) => link.href === e.target.href);
+
+        addActive(neededEl, headerNav);
+
+
+        const blockId = e.target.getAttribute('href').substring(1);
+
+        document.getElementById(blockId).scrollIntoView({
+            behavior: "smooth",
+            block: "start"
+        });
+    });
+
+    function addActive(element, parent, classOfItems = 'nav__item') {
+        const items = parent.querySelectorAll(`.${classOfItems}`);
+        items.forEach((item) => {
+            item.classList.remove('active');
+        });
+        element.classList.add('active');
+    }
+});
