14e5b81cd777c987f01c8990a484b47d191fb186
ivanchikap <anton.ivanchik@gmail.com>
2022-11-17 08:00:35 +0200

    during 2 slider

--- src/assets/js/init.js
+++ src/assets/js/init.js
@@ -23,7 +23,33 @@ $( document ).ready(function() {
         centerPadding: '',
         slidesToShow: 3,
         responsive: [
+            {
+                breakpoint: 992,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 2,
+                    centerPadding: '230px'
+                }
+            },
 
+            {
+                breakpoint: 780,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    centerPadding: '120px'
+                }
+            },
+
+            {
+                breakpoint: 576,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    centerPadding: '10px',
+                    arrows: false
+                }
+            },
         ]
     });
 });
